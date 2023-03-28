42f600dd5bf3155b1debb2228902099db7bbb688
rhquddnr95 <39820865+rhquddnr95@users.noreply.github.com>
2023-03-29 03:50:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,53 @@
+function solution(s) {
+    const eng3 = ["one","two","six"];
+    const eng4 = ["zero","five","four","nine"];
+    const eng5 = ["three","seven","eight"];
+    
+    let answer=[];
+    for(let i = 0; i<s.length; i++){
+        if(s[i]%1===0){
+            answer.push(s[i]);
+        }
+        else if(s.slice(i,i+3)==="one"){
+            answer.push(1);
+            i=i+2;
+        }
+        else if(s.slice(i,i+3)==="two"){
+            answer.push(2);
+            i=i+2;
+        }
+        else if(s.slice(i,i+3)==="six"){
+            answer.push(6);
+            i=i+2;
+        }
+        else if(s.slice(i,i+4)==="zero"){
+            answer.push(0);
+            i=i+3;
+        }
+        else if(s.slice(i,i+4)==="nine"){
+            answer.push(9);
+            i=i+3;
+        }
+        else if(s.slice(i,i+4)==="five"){
+            answer.push(5);
+            i=i+3;
+        }
+        else if(s.slice(i,i+4)==="four"){
+            answer.push(4);
+            i=i+3;
+        }
+        else if(s.slice(i,i+5)==="three"){
+            answer.push(3);
+            i=i+4;
+        }
+        else if(s.slice(i,i+5)==="seven"){
+            answer.push(7);
+            i=i+4;
+        }
+        else if(s.slice(i,i+5)==="eight"){
+            answer.push(8);
+            i=i+4;
+        }
+    }
+    return answer.join("")/1;
+}
