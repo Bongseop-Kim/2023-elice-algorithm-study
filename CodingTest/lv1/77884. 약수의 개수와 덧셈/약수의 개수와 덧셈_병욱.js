fe2b15005dd76be31e44b5d16b52e38b4da2f481
rhquddnr95 <39820865+rhquddnr95@users.noreply.github.com>
2023-03-29 10:10:35 +0900

    약수의 개수 개선
    약수의 개수 개선

--- CodingTest/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈_병욱.js
+++ CodingTest/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈_병욱.js
@@ -2,7 +2,7 @@ function solution(left, right) {
     var answer = 0;
     for(let i=left; i<=right; i++){
         console.log(divisor_num(i));
-        if(divisor_num(i)%2===0){
+        if(divisor_num(i)%1!==0){
             answer+=i;
         }
         else{
@@ -13,11 +13,5 @@ function solution(left, right) {
 }
 
 function divisor_num(num){
-    let count=0;
-    for(let i = 1; i<=num; i++){
-        if(num%i===0){
-            count++;
-        }
-    }
-    return count;
+    return Math.sqrt(num);
 }
