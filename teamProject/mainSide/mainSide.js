2dc1fffa2c817f721352b39b8db403629b22e30f
김득열 <emrfufrns@gmail.com>
2023-04-06 01:26:42 +0900

    mainside.js 수정

--- teamProject/mainSide/mainSide.js
+++ teamProject/mainSide/mainSide.js
@@ -1,14 +1,4 @@
 const images = ['log.png', 'diamond.png', 'stone.png'];
-<<<<<<< HEAD
-const mainSide = document.querySelector('.mainSide');
-const topContainer = document.querySelector('.top');
-const bottomContainer = document.querySelector('.bottom');
-
-
-const moneyBagButton = createButton('money-bag.png');
-topContainer.appendChild(moneyBagButton);
-
-=======
 const moneyBagButton = createButton('money-bag.png');
 const mainSide = document.querySelector('.mainSide');
 const topContainer = document.createElement('div');
@@ -20,22 +10,10 @@ mainSide.appendChild(topContainer);
 const bottomContainer = document.createElement('div');
 bottomContainer.classList.add('bottom-container');
 mainSide.appendChild(bottomContainer);
->>>>>>> 3e0cbaba5f6e52bbd44cd78e5a068da7a6ab24e1
 
 function createButton(image) {
     const button = document.createElement('img');
     button.src = `./assets/${image}`;
-<<<<<<< HEAD
-
-    return button;
-}
-
-
-images.forEach(image => {
-    const button = createButton(image);
-    bottomContainer.appendChild(button);
-});
-=======
     return button;
 }
 
@@ -43,4 +21,3 @@ images.forEach(image => {
     const button = createButton(image);
     bottomContainer.appendChild(button);
 });
->>>>>>> 3e0cbaba5f6e52bbd44cd78e5a068da7a6ab24e1
