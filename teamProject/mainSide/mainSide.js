04323d83f70ca907fc7d2733d6208671554f9108
김득열 <emrfufrns@gmail.com>
2023-04-05 23:27:54 +0900

    230405_mainSide 기본 영역 작업_득열

--- teamProject/mainSide/mainSide.js
+++ teamProject/mainSide/mainSide.js
@@ -1,3 +1,22 @@
-var a = 1;
-var b = 1;
-var c = 1;
+const images = ['log.png', 'diamond.png', 'stone.png'];
+const mainSide = document.querySelector('.mainSide');
+const topContainer = document.querySelector('.top');
+const bottomContainer = document.querySelector('.bottom');
+
+
+const moneyBagButton = createButton('money-bag.png');
+topContainer.appendChild(moneyBagButton);
+
+
+function createButton(image) {
+    const button = document.createElement('img');
+    button.src = `./assets/${image}`;
+
+    return button;
+}
+
+
+images.forEach(image => {
+    const button = createButton(image);
+    bottomContainer.appendChild(button);
+});
