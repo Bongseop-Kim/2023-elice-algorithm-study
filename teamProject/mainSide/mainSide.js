3e0cbaba5f6e52bbd44cd78e5a068da7a6ab24e1
김득열 <emrfufrns@gmail.com>
2023-04-05 23:58:33 +0900

    html 수정없이 mainSide 작업

--- teamProject/mainSide/mainSide.js
+++ teamProject/mainSide/mainSide.js
@@ -1,20 +1,21 @@
 const images = ['log.png', 'diamond.png', 'stone.png'];
-const mainSide = document.querySelector('.mainSide');
-const topContainer = document.querySelector('.top');
-const bottomContainer = document.querySelector('.bottom');
-
-
 const moneyBagButton = createButton('money-bag.png');
-topContainer.appendChild(moneyBagButton);
+const mainSide = document.querySelector('.mainSide');
+const topContainer = document.createElement('div');
 
+topContainer.classList.add('top-container');
+topContainer.appendChild(moneyBagButton);
+mainSide.appendChild(topContainer);
+
+const bottomContainer = document.createElement('div');
+bottomContainer.classList.add('bottom-container');
+mainSide.appendChild(bottomContainer);
 
 function createButton(image) {
     const button = document.createElement('img');
     button.src = `./assets/${image}`;
-
     return button;
 }
-
 
 images.forEach(image => {
     const button = createButton(image);
