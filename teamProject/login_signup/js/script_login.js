1d66f8e454c28d46537f2026f3d04b639522f5aa
uniqueflowSA <saco2020a@gmail.com>
2023-04-08 23:33:02 +0900

    commit

--- teamProject/login_signup/js/script_login.js
+++ teamProject/login_signup/js/script_login.js
@@ -4,14 +4,14 @@ const idInput = document.querySelector('#id_input') //아이디입력
 const psInput = document.querySelector('#ps_input') //비밀번호입력
 const loginBtn = document.querySelector('#login_btn') //회원가입버튼
 const signupPage = document.querySelector('#signup_link_btn')
-// const signupFunc = ()=>{
+// const loginupFunc = ()=>{
 
 // }
 
 
 // loginForm.addEventListener('submit', loginFunc)
 signupPage.addEventListener('click', (e)=>{
-    e.preventDefault
+    e.preventDefault()
     window.location.href = 'signup.html'
     console.log("click")
 })
