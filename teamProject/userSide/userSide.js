0258d914f04e65f34267b2760e8a330eac62d4d6
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-04-09 20:30:09 +0900

    봉섭

--- teamProject/userSide/userSide.js
+++ teamProject/userSide/userSide.js
@@ -10,37 +10,38 @@
 //   money:0
 // }]
 
-const userName = document.querySelector('#userName')
-const logButton = document.querySelector('#userInfo button')
-const userPower = document.querySelector('#userPower')
-const userMoney = document.querySelector('#userMoney')
+const userName = document.querySelector("#userName");
+const logButton = document.querySelector("#userName button");
+const userPower = document.querySelector("#userPower");
+const userMoney = document.querySelector("#userMoney");
 
 const userInfoUpdate = (currentUser) => {
   const user = currentUser[0];
-  document.getElementById("userInfo").childNodes[0].nodeValue = user.name;
+  document.getElementById("userName").childNodes[0].nodeValue = user.name;
   userPower.innerHTML = user.power;
   userMoney.innerHTML = user.money;
   logButton.innerHTML = "로그아웃";
-}
+};
 
 const loginCheck = () => {
-  fetch('/api/users')
-    .then(res => res.json())
-    .then(data => {
-      if(data.currentUser) {
-        userInfoUpdate(data.currentUser);
+  fetch("https://port-0-king-of-mine-1093j2alg6lmfjz.sel3.cloudtype.app/api/users/getTopTenUsers")
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.data) {
+        console.log(data.data);
+        userInfoUpdate(data.data);
       } else {
-        window.location.href = '로그인 페이지';
+        // window.location.href = "/login";
       }
     })
-    .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
 
 loginCheck();
 
-logButton.addEventListener('click', () => {
-  if (confirm('로그아웃 하시겠습니까?')) {
-    window.location.href = '로그인 페이지';
+logButton.addEventListener("click", () => {
+  if (confirm("로그아웃 하시겠습니까?")) {
+    window.location.href = "/login";
   } else {
     return;
   }
