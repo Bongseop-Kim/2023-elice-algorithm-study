fe950e7567bb0d5e13f3d8c2b80440f60cf40382
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-04-10 21:21:51 +0900

    봉섭

--- teamProject/mainSide/mainSide.js
+++ teamProject/mainSide/mainSide.js
@@ -1,27 +1,24 @@
-<<<<<<< HEAD
-=======
 // 커밋 테스트를 위한 임시 주석
-const images = ['log.png', 'diamond.png', 'stone.png'];
-const moneyBagButton = createButton('money-bag.png');
-const mainSide = document.querySelector('.mainSide');
-const topContainer = document.createElement('div');
+const images = ["log.png", "diamond.png", "stone.png"];
+const moneyBagButton = createButton("money-bag.png");
+const mainSide = document.querySelector(".mainSide");
+const topContainer = document.createElement("div");
 
-topContainer.classList.add('top-container');
+topContainer.classList.add("top-container");
 topContainer.appendChild(moneyBagButton);
 mainSide.appendChild(topContainer);
 
-const bottomContainer = document.createElement('div');
-bottomContainer.classList.add('bottom-container');
+const bottomContainer = document.createElement("div");
+bottomContainer.classList.add("bottom-container");
 mainSide.appendChild(bottomContainer);
 
 function createButton(image) {
-    const button = document.createElement('img');
-    button.src = `./assets/${image}`;
-    return button;
+  const button = document.createElement("img");
+  button.src = `./assets/${image}`;
+  return button;
 }
 
-images.forEach(image => {
-    const button = createButton(image);
-    bottomContainer.appendChild(button);
+images.forEach((image) => {
+  const button = createButton(image);
+  bottomContainer.appendChild(button);
 });
->>>>>>> e614403db03536dae73d6f2196290607fee47016
--- teamProject/userSide/userSide.js
+++ teamProject/userSide/userSide.js
@@ -1,63 +1,68 @@
-// html 
-const userSide_box = document.querySelector('.userSide.box');
-const userName = document.createElement('div');
-userName.setAttribute('id', 'userName');
+let user = {
+  name: "",
+  strong: 0,
+  money: 0,
+};
+
+const userSide_box = document.querySelector(".userSide.box");
+const userName = document.createElement("div");
+userName.setAttribute("id", "userName");
 userSide_box.appendChild(userName);
 
-const userRankList = document.createElement('ul');
-userRankList.setAttribute('class', 'userRankList');
+const userRankList = document.createElement("ul");
+userRankList.setAttribute("class", "userRankList");
 userName.appendChild(userRankList);
 
-const rankImg = document.createElement('img');
-rankImg.setAttribute('src', './assets/ranking.png');
-rankImg.setAttribute('alt', 'rankingImg');
-rankImg.setAttribute('id', 'rankImg');
+const rankImg = document.createElement("img");
+rankImg.setAttribute("src", "./assets/ranking.png");
+rankImg.setAttribute("alt", "rankingImg");
+rankImg.setAttribute("id", "rankImg");
 userName.appendChild(rankImg);
 
-const logButton = document.createElement('div');
-logButton.setAttribute('id', 'logButton');
+const logButton = document.createElement("div");
+logButton.setAttribute("id", "logButton");
 userName.appendChild(logButton);
 
-const userPower = document.createElement('div');
-userPower.setAttribute('id', 'userPower');
+const userPower = document.createElement("div");
+userPower.setAttribute("id", "userPower");
 userName.appendChild(userPower);
 
-const userMoney = document.createElement('div');
-userMoney.setAttribute('id', 'userMoney');
+const userMoney = document.createElement("div");
+userMoney.setAttribute("id", "userMoney");
 userName.appendChild(userMoney);
 
 // userSide top
-const userInfoUpdate = (currentUser) => {
-  const user = currentUser[0];
-  document.getElementById("userInfo").childNodes[0].nodeValue = user.name;
-  userPower.innerHTML = user.power;
+const userInfoUpdate = (user) => {
+  console.log(user);
+  document.getElementById("userName").childNodes[0].nodeValue = user.name;
+  userPower.innerHTML = user.strong;
   userMoney.innerHTML = user.money;
   logButton.innerHTML = "로그아웃";
-}
+};
 
 const loginCheck = () => {
-  fetch('https://port-0-king-of-mine-1093j2alg6lmfjz.sel3.cloudtype.app/api/users', {
-    method: 'GET',
+  fetch("https://port-0-king-of-mine-1093j2alg6lmfjz.sel3.cloudtype.app/api/users", {
+    method: "GET",
     headers: {
-      Authorization: Bearer //token,
-    }
+      Authorization: `Bearer ${Cookies.get().token}`,
+    },
   })
-    .then(res => res.json())
-    .then(data => {
-      if(data.data) {
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.success) {
         userInfoUpdate(data.data);
       } else {
-        window.location.href = '로그인 페이지';
+        // window.location.href = "로그인 페이지";
       }
     })
-    .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
 
 loginCheck();
 
-logButton.addEventListener('click', () => {
-  if (confirm('로그아웃 하시겠습니까?')) {
-    window.location.href = '로그인 페이지';
+logButton.addEventListener("click", () => {
+  if (confirm("로그아웃 하시겠습니까?")) {
+    window.location.href = "로그인 페이지";
   } else {
     return;
   }
@@ -65,20 +70,22 @@ logButton.addEventListener('click', () => {
 
 //userSide bottom
 const topTenUserSort = (data) => {
-  data.data.sort((a,b) => a.money - b.money)
+  data.data.sort((a, b) => a.money - b.money);
   return data.data.slice(0, 10);
-}
+};
 
 const showTopTen = () => {
-  fetch('https://port-0-king-of-mine-1093j2alg6lmfjz.sel3.cloudtype.app/api/users/getTopTenUsers')
-    .then(res => res.json())
-    .then(data => {
+  fetch("https://port-0-king-of-mine-1093j2alg6lmfjz.sel3.cloudtype.app/api/users/getTopTenUsers")
+    .then((res) => res.json())
+    .then((data) => {
       const topTenUsers = topTenUserSort(data);
-      topTenUsers.forEach(user => {
+      topTenUsers.forEach((user) => {
         const rankList = document.createElement("li");
         rankList.innerText = user.name + user.money;
         userRankList.appendChild(rankList);
       });
     })
-    .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
+
+showTopTen();
