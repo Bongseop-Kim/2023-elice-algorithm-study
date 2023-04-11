78123db1ce4045e0f5c027dbaf2660883c977cbe
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-04-11 20:32:53 +0900

    봉섭

--- teamProject/login_signup/js/script_login.js
+++ teamProject/login_signup/js/script_login.js
@@ -1,12 +1,14 @@
 const logImg = document.querySelector("#logoImg");
 const loginForm = document.querySelector("#login_form"); //회원가입폼 전체
-const idInput = document.querySelector("#id_input"); //아이디입력
+const emailInput = document.querySelector("#email_input"); //아이디입력
 const psInput = document.querySelector("#ps_input"); //비밀번호입력
 const loginBtn = document.querySelector("#login_btn"); //회원가입버튼
 const signupPage = document.querySelector("#signup_link_btn");
-const loginFunc = () => {
-  idInput.value === "" //이메일 빈칸일경우
-    ? (alert("이메일을 입력해주세요."), (idInput.value = null))
+
+const loginFunc = (e) => {
+  e.preventDefault();
+  emailInput.value === "" //이메일 빈칸일경우
+    ? (alert("이메일을 입력해주세요."), (emailInput.value = null))
     : psInput.value === "" //비밀번호 빈칸일경우
     ? (alert("비밀번호를 입력해주세요."), (psInput.value = null))
     : null;
@@ -17,7 +19,7 @@ const loginFunc = () => {
       "content-Type": "application/json",
     },
     body: JSON.stringify({
-      email: idInput.value, //전송정보 1. 이메일
+      email: emailInput.value, //전송정보 1. 이메일
       password: psInput.value, //전송정보 2. 비밀번호
     }),
   })
@@ -27,7 +29,7 @@ const loginFunc = () => {
         //로그인 완료시
         Cookies.set("token", data.data.token); // 로그인 정보 쿠키에 저장
         alert("로그인 완료되었습니다.");
-        window.location.href = "index.html";
+        window.location.href = "/teamProject/index.html";
       } else {
         throw new Error("로그인 실패했습니다."); //로그인실패 에러 생성
       }
@@ -35,11 +37,10 @@ const loginFunc = () => {
     .catch((error) => {
       //에러시 메세지
       console.error(error);
-      alert("로그인에 실패했습니다.");
+      // alert("로그인에 실패했습니다.");
     });
 };
 
-loginForm.addEventListener("submit", loginFunc);
 signupPage.addEventListener("click", (e) => {
   //회원가입 클릭시 회원가입페이지 이동
   e.preventDefault();
@@ -48,5 +49,8 @@ signupPage.addEventListener("click", (e) => {
 });
 logImg.addEventListener("click", () => {
   //로고클릭시 메인페이지 이동
-  window.location.href = "https://port-0-king-of-mine-1093j2alg6lmfjz.sel3.cloudtype.app/";
+  window.location.href = "/teamProject/index.html";
+});
+loginBtn.addEventListener("click", (e) => {
+  loginFunc(e);
 });
--- teamProject/login_signup/js/script_signup.js
+++ teamProject/login_signup/js/script_signup.js
@@ -1,7 +1,7 @@
 //회원가입 요소 불러오기
 const logImg = document.querySelector("#logoImg");
 const signupForm = document.querySelector("#signup_form"); //회원가입폼 전체
-const idInput = document.querySelector("#id_input"); //아이디입력
+const emailInput = document.querySelector("#email_input"); //아이디입력
 const nameInput = document.querySelector("#name_input"); //닉네임입력
 const psInput = document.querySelector("#ps_input1"); //비밀번호입력
 const psConfirm = document.querySelector("#ps_input2"); //비밀번호확인
@@ -10,8 +10,8 @@ const signupBtn = document.querySelector("#sinup_btn"); //회원가입버튼
 const signupFunc = (event) => {
   event.preventDefault();
 
-  idInput.value === "" //이메일 빈칸일경우
-    ? (alert("이메일을 입력해주세요."), (idInput.value = null))
+  emailInput.value === "" //이메일 빈칸일경우
+    ? (alert("이메일을 입력해주세요."), (emailInput.value = null))
     : nameInput.value === "" //닉네임 빈칸일경우
     ? (alert("닉네임을 입력해주세요."), (nameInput.value = null))
     : psInput.value === "" //비밀번호 빈칸일경우
@@ -26,7 +26,7 @@ const signupFunc = (event) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      email: idInput.value,
+      email: emailInput.value,
       name: nameInput.value,
       password: psInput.value,
     }),
@@ -34,6 +34,7 @@ const signupFunc = (event) => {
     .then((response) => {
       if (response.ok) {
         alert("회원가입이 완료되었습니다.");
+        window.location.href = "/teamProject/login_signup/login.html";
       } else {
         throw new Error("회원가입에 실패했습니다.");
       }
@@ -48,5 +49,5 @@ signupForm.addEventListener("submit", signupFunc);
 
 logImg.addEventListener("click", () => {
   //로고클릭시 메인페이지이동
-  window.location.href = "https://port-0-king-of-mine-1093j2alg6lmfjz.sel3.cloudtype.app/";
+  window.location.href = "/teamProject/index.html";
 });
